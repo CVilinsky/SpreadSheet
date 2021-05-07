6f510263bd3580a6a105e471fd5560911101e374
Chen Vilinsky <vilinsky@post.bgu.ac.il>
2021-05-07 16:53:21 +0300

    Added stuff to Class1

--- Class1.cs
+++ Class1.cs
@@ -18,6 +18,27 @@ namespace HW3
 			}
 			
 		}
+		public String OP(string func)
+		{
+
+			return "HI";
+		}
+
+		public static string[] getFunc(int nOperations)
+		{
+			string[] funcList = new String[nOperations];
+			string[] randL = new string[12] { "save", "getSize", "addColl","addRow", "searchInRange", "getCell", "setCell",
+											 "searchString", "exchangeRows", "exchangeCols", "searchInRow", "searchInCol" };
+			Random rnd = new Random();
+			for (int i = 0; i < nOperations; i++)
+			{
+				string item = randL[rnd.Next(randL.Length)];
+				funcList[i] = item;
+
+			}
+
+			return funcList;
+		}
 		public static void DoTasks()
 		{
 			string[] rand_oper = new string[10];
